240aabb2e834dadd103a7cf06cc47c41b785e851
chandrima925 <chandrimasengupta925@gmail.com>
2024-03-29 20:28:15 +0530

    updating

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ toggleButton.addEventListener("click", function () {
         icon.classList.remove("ri-eye-line");
     }
 })
-
+ 
 // ------------------------------------------------ 
 
 // password length
